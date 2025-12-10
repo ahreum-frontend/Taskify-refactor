9e7cca2ec52c128dc0252a6a0ea85e24aac914cb
ahreum <117738875+aahreum@users.noreply.github.com>
2025-12-10 22:18:07 +0900

    [⚙️ Chore/1] 프로젝트 기초 세팅 진행 (#2)
    * ⚙️ Chore: gitignore 추가
    
    * ⚙️ Chore: 프리티어 설정 파일 추가
    
    * ⚙️ Chore: 린트 설정 추가
    
    * ⚙️ Chore: 타입스크립트 설정 추가
    
    * ⚙️ Chore: 커밋 린트 설정 추가
    
    * ⚙️ Chore: husky 훅 추가
    
    * ⚙️ Chore: vite 프로젝트 생성

---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import importPlugin from 'eslint-plugin-import';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules']),
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      importPlugin.flatConfigs.recommended,
+    ],
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.app.json',
+        },
+      },
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'react/prop-types': 'off',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-pascal-case': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'import/no-unresolved': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          'newlines-between': 'never',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      curly: ['error'],
+      'no-var': 'error',
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+    },
+  },
+]);
